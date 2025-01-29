2a4a0efcb46bc9e87e5fd500d45911477fa1466d
ankitmishra06 <ankitkumar011001@gmail.com>
2025-01-29 17:13:12 +0530

    complete model trainer components and mlflow tracking

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import (DataIngestionConfig, TrainingPipelineConfig,
+                                                   DataValidationConfig, DataTransformationConfig,
+                                                     ModelTrainerConfig)
+
+from networksecurity.components.model_trainer import ModelTrainer
 import sys
 
 # Main entry point for the script
@@ -41,6 +45,12 @@ if __name__=="__main__":
         data_transformation_artifact=data_tranformation.initiate_data_transformation()
         logging.info("Data transformation completed")
         print(data_transformation_artifact)
+
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        logging.info("Model Training started")
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Modle Training completed and artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
